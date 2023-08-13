5267ab79dffc9e7ef4bdcb23eba33c16185e6477
uday-rana <uday-rana@users.noreply.github.com>
2023-08-13 23:14:21 +0000

    Prettified Code!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./views/*.hbs',
-            './views/layouts/*.hbs'],
+  content: ["./views/*.hbs", "./views/layouts/*.hbs"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
-}
-
+};
