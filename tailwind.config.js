e8ac0ff4b42b6dc0f539a340dbe452d54cc34f05
uday-rana <82779467+uday-rana@users.noreply.github.com>
2023-08-20 02:33:33 -0400

    Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ export default {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["corporate", "business"],
+    themes: ["cupcake", "forest"],
   },
 };
